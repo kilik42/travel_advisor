cce8f34e1a94281bf29d41bdb0851da98db029b7
marvin evins <marvinevins@yahoo.com>
2023-05-14 13:22:21 -0500

    finishing home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
+import { Image } from 'react-native';
+import { HeroImage } from '../assets';
 const HomeScreen = () => {
 
   const navigation = useNavigation();
@@ -34,9 +36,25 @@ const HomeScreen = () => {
           <Text className="text-[#2A2B4B] text-[42px]">Enjoy the trip with</Text>
           <Text className="text-[#00BCC9] text-[38px] font-bold">Good moments</Text>
 
-          <Text>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus odio temporibus accusantium culpa. Molestiae accusamus ducimus deleniti nesciunt soluta labore aspernatur fugiat nulla optio alias, accusantium atque cumque. Molestiae, provident.
+          <Text className="text-[#3C6072] text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
           </Text>
+          </View>
+
+          {/* third section  circle section*/}
+          <View className="w-[400px] h-[400px] bg-[#00BCC9] rounded-full absolute bottom-36 -right-36">
+          </View>
+          <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36">
+          </View>
+
+          {/* image container */}
+          <View className="flex-1 relative items-center justify-center ">
+
+           <Image
+              className ="w-full h-full object-cover mt-20"
+
+              source ={HeroImage}
+             />
           </View>
 
       </SafeAreaView>
