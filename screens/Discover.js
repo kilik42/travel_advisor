1e4f67da96802b7c8d77f970224f4e069b9e981e
marvin evins <marvinevins@yahoo.com>
2023-05-14 17:19:06 -0500

    working on discover page

---
+++ screens/Discover.js
@@ -0,0 +1,43 @@
+import { View, Text,SafeAreaView,Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import { useLayoutEffect } from 'react';
+import Avatar from '../assets/avatar.png'
+import React from 'react'
+
+const Discover = () => {
+    const navigation = useNavigation();
+    useLayoutEffect(() => {
+        navigation.setOptions({
+          headerShown: false,
+          // title: 'Home',
+          // headerStyle: { backgroundColor: '#fff' },
+          // headerTitleStyle: { color: 'black' },
+          // headerTintColor: 'black',
+        }, []);
+      })
+  return (
+    <SafeAreaView className ="flex-1 bg-white relative">
+        {/* first section */}
+        <View className="flex-row items-center justify-between px-8">
+            <View>
+                <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+                <Text className="text-[#527873] text-[36px]">the beauty today</Text>
+                {/* <Text>Discover</Text> */}
+            </View>
+            <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
+                <Image
+                className="w-full h-full rounded-md object-cover"
+                    source={Avatar}
+                
+                />
+            </View>
+            
+
+
+        </View>
+      
+    </SafeAreaView>
+  )
+}
+
+export default Discover
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { Image } from 'react-native';
 import { HeroImage } from '../assets';
+import * as Animatable from 'react-native-animatable';
+
 const HomeScreen = () => {
 
   const navigation = useNavigation();
@@ -50,12 +52,34 @@ const HomeScreen = () => {
           {/* image container */}
           <View className="flex-1 relative items-center justify-center ">
 
-           <Image
+           <Animatable.Image
+              animation="pulse"
+              easing="ease-in-out"
               className ="w-full h-full object-cover mt-20"
 
               source ={HeroImage}
              />
+
+        <TouchableOpacity 
+        onPress={() => navigation.navigate('Discover')}
+        className='absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounded-full items-center justify-center'>
+          
+                    <Animatable.View  
+                    animation="pulse"
+                    easing="ease-in-out"
+                    iterationCount="infinite"
+                    className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
+                      <Text className="text-gray-50 text-[40px] font-semibold" >
+
+                        Go
+                      </Text>
+                    </Animatable.View>
+            
+        </TouchableOpacity>
+
           </View>
+
+          
 
       </SafeAreaView>
 
